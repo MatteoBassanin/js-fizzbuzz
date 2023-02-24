353c4f21a58e8d54511b5962299fbfc41496332e
MatteoBassanin <matteo.bassanin@gmail.com>
2023-02-24 16:57:27 +0100

    tidying the code

--- js/main.js
+++ js/main.js
@@ -4,29 +4,21 @@ const containerDom = document.querySelector(".container");
 for (let i = 1; i <= 100; i++ ) {
     const quadratino = document.createElement("div");
     quadratino.classList.add("square");
-    // quadratino.append(i);
     containerDom.append(quadratino);
 
 
 
 
     if ( i%15 == 0){
-        // console.log("fizzbuzz")
         quadratino.append("fizzbuzz");
-        // quadratino.classList.remove("square");
         quadratino.classList.add("square_fizzbuzz");
     } else if ( i%3 == 0){       
-        // console.log("fizz");
         quadratino.append("fizz");
-        // quadratino.classList.remove("square");
         quadratino.classList.add("square_fizz");        
     } else if ( i%5 == 0){
-        // console.log("buzz")
         quadratino.append("buzz");
-        // quadratino.classList.remove("square");
         quadratino.classList.add("square_buzz");
     }else{
-        // console.log(i);
         quadratino.append(i);
         quadratino.classList.add("simple");
         
