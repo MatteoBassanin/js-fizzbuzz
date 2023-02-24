a3b9ad7fb0286cd0fe3161f0870739aebeef94d4
MatteoBassanin <matteo.bassanin@gmail.com>
2023-02-24 16:17:09 +0100

    fixed divider by 15 and start to work on the graph

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
+const containerDom = document.querySelector(".container");
+
+
 for (let i = 1; i <= 100; i++ ) {
-    
+    const quadratino = document.createElement("div");
+    quadratino.classList.add("square");
+    quadratino.append(i);
+    containerDom.append(quadratino);
 
 
 
 
-    
-    if ( i%3 == 0){       
+    if ( i%15 == 0){
+        console.log("fizzbuzz")
+    } else if ( i%3 == 0){       
         console.log("fizz");        
     } else if ( i%5 == 0){
         console.log("buzz")
-    } else if ( i%15 == 0){
-        console.log("fizzbuzz")
     }else{
         console.log(i);
     }
